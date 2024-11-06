538c7518c0f885d211ce97c56d4c4257441e98fb
laklov <lasse.klovstad@gmail.com>
2024-11-06 14:21:44 +0100

    Try setting NODE_ENV=test

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,5 +75,8 @@ export default defineConfig({
     command: "npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
+    env: {
+      NODE_ENV: "test",
+    },
   },
 });
